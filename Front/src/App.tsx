10fc08fca0628f53b361fbb1f6a8129c536393ee
Gonzadevolper <gonza.cay@gmail.com>
2024-05-08 19:10:08 -0300

    Front base con Typescript-SWC y Bootstrap instlados funcionando localmente

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -5,7 +5,8 @@ function App() {
 
   return (
     <>
-      
+      <h1 className="display-1">Funcionando</h1> {/* Utiliza una clase de Bootstrap para el estilo del título */}
+      <button className="btn btn-primary">Botón</button> {/* Utiliza clases de Bootstrap para el estilo del botón */}
     </>
   )
 }
