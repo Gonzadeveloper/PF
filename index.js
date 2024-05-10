0fc261f152de62b7334655326d5bd0013dd53029
GPWaterdrinker <pinedogonzalo96@gmail.com>
2024-05-10 13:55:13 -0300

    base de datos conectada

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from "express";
+import {config} from 'dotenv';
+import pg from 'pg';
 
+config()
 
 const app = express ()
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/ping', async (req, res) => {
+    const result = await  pool.query('SELECT NOW()' )
+    return res.json(result.rows[0])
 })
 
 app.listen(3000)
