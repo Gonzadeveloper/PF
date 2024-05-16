dca1e419a66e363f2e7f0ad888b31b7c81c21718
alvarorojas1101 <alvarorojas1101@gmail.com>
2024-05-16 13:26:53 -0300

    Creacion vista MiPerfil, con inicio de sesión y creación de cuenta aun sin la funcionalidad

--- Front/src/Components/Search/Search.tsx
+++ Front/src/Components/Search/Search.tsx
@@ -1,43 +1,42 @@
-import Card from "../Card/Card";
-import React from 'react';
-import { connect } from 'react-redux';
-import { RootState } from '../../Redux/index';
-import { Article } from '../../Redux/Slices/ArticlesSlice'
-
+import Card from "../Card/card";
+import React from "react";
+import { connect } from "react-redux";
+import { RootState } from "../../Redux/index";
+import { Article } from "../../Redux/Slices/ArticlesSlice";
 
 interface Props {
-    articles: Article[];
-  }
+  articles: Article[];
+}
 
-  const Search: React.FC<Props> = ({ articles }) => {
-    return (
-        <div className="container">
+const Search: React.FC<Props> = ({ articles }) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2">
+          <h4>Filtros</h4>
+        </div>
+        <div className="col-md-10">
           <div className="row">
-            <div className="col-md-2">
-              <h4>Filtros</h4>
-            </div>
-            <div className="col-md-10">
-              <div className="row">
-                {articles.map((card) => (
-                  <Card
-                    key={card.id}
-                    id={card.id}
-                    img={card.img}
-                    title={card.title}
-                    content={card.content}
-                    price={card.price}
-                    ratings={card.ratings}
-                  />
-                ))}
-              </div>
-            </div>
+            {articles.map((card) => (
+              <Card
+                key={card.id}
+                id={card.id}
+                img={card.img}
+                title={card.title}
+                content={card.content}
+                price={card.price}
+                ratings={card.ratings}
+              />
+            ))}
           </div>
         </div>
-      );
-    };
-  
-  const mapStateToProps = (state: RootState) => ({
-    articles: state.articles.list, // Suponiendo que tienes un slice llamado articles en tu store
-  });
-  
-  export default connect(mapStateToProps)(Search);
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: RootState) => ({
+  articles: state.articles.list, // Suponiendo que tienes un slice llamado articles en tu store
+});
+
+export default connect(mapStateToProps)(Search);
