96b411b73561862f6f2972c590152d4be3d5710b
alvarorojas1101 <alvarorojas1101@gmail.com>
2024-05-16 16:56:21 -0300

    Eliminacion por el momento de autenticacion con google

--- Front/src/Components/Search/Search.tsx
+++ Front/src/Components/Search/Search.tsx
@@ -1,35 +1,35 @@
-import Card from "../Card/Card";
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { CardProps } from '../Card/Card';
-import { RootState } from '../../Redux/index';
+import Card from "../Card/card";
+import React from "react";
+import { useSelector } from "react-redux";
+import { CardProps } from "../Card/Card";
+import { RootState } from "../../Redux/index";
 
 function Search() {
   const products = useSelector((state: RootState) => state.products.products);
 
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-2">
-            <h4>Filtros</h4>
-          </div>
-          <div className="col-md-10">
-            <div className="row">
-              {products.map((card) => (
-                <Card
-                  key={card.id}
-                  id={card.id}
-                  image={card.image}
-                  name={card.name}
-                  description={card.description}
-                  price={card.price}
-                />
-              ))}
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2">
+          <h4>Filtros</h4>
+        </div>
+        <div className="col-md-10">
+          <div className="row">
+            {products.map((card) => (
+              <Card
+                key={card.id}
+                id={card.id}
+                image={card.image}
+                name={card.name}
+                description={card.description}
+                price={card.price}
+              />
+            ))}
           </div>
         </div>
       </div>
-    );
-};
+    </div>
+  );
+}
 
 export default Search;
