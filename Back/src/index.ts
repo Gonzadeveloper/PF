69803ea5dce90c169dbc88a66da6a5378ce502b2
MGoretta <maugoretta@gmail.com>
2024-05-20 10:36:30 -0300

    anulada conex postgres para habilitar la correcta

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -1,29 +1,29 @@
-import express from "express"
-import getAllProducts  from './routes/getAllProducts' ;
-import  getProductByName from "./routes/getProductByName";
-import getProductById from "./routes/getProductById";
+// import express from "express"
+// import getAllProducts  from './routes/getAllProducts' ;
+// import  getProductByName from "./routes/getProductByName";
+// import getProductById from "./routes/getProductById";
 
-const app = express()
-app.use(express.json()) // middleware que transforma la req.body a un json
+// const app = express()
+// app.use(express.json()) // middleware que transforma la req.body a un json
 
-const PORT = 3000
+// const PORT = 3000
 
-import db from "../models";
-//import db = require("../models");
+// import db from "../models";
+// //import db = require("../models");
 
-db.sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-    })
-})
+// db.sequelize.sync().then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`Server running on port ${PORT}`)
+//     })
+// })
 
-app.get ('/products', getAllProducts);
-app.get ('/products/:name', getProductByName);
-app.get ('/products/:id', getProductById);
+// app.get ('/products', getAllProducts);
+// app.get ('/products/:name', getProductByName);
+// app.get ('/products/:id', getProductById);
 
 
-app.get('/ping', (_req, res) => {
-    console.log('someone pinged here!!');
-    res.send('pong');
-});
+// app.get('/ping', (_req, res) => {
+//     console.log('someone pinged here!!');
+//     res.send('pong');
+// });
 
--- Back/src/sequelize.ts
+++ Back/src/sequelize.ts
@@ -1,60 +1,60 @@
-import { Sequelize } from 'sequelize-typescript';
-import path from 'path';
-import dotenv from 'dotenv';
-dotenv.config();
+// import { Sequelize } from 'sequelize-typescript';
+// import path from 'path';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-// Validación de variables de entorno
-function validateEnvVariables() {
-  const requiredEnvVars = ['DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_NAME'];
-  requiredEnvVars.forEach((varName) => {
-    if (!process.env[varName]) {
-      throw new Error(`Variable de entorno ${varName} no está definida.`);
-    }
-  });
-}
+// // Validación de variables de entorno
+// function validateEnvVariables() {
+//   const requiredEnvVars = ['DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_NAME'];
+//   requiredEnvVars.forEach((varName) => {
+//     if (!process.env[varName]) {
+//       throw new Error(`Variable de entorno ${varName} no está definida.`);
+//     }
+//   });
+// }
 
-validateEnvVariables();
+// validateEnvVariables();
 
-const { URL } = process.env;
+// const { URL } = process.env;
 
-if (!URL) {
-  throw new Error("La variable de entorno URL no está definida en el archivo.env");
-}
+// if (!URL) {
+//   throw new Error("La variable de entorno URL no está definida en el archivo.env");
+// }
 
-const sequelize = new Sequelize(URL, {
-  dialect: "postgres",
-  models: [path.join(__dirname, "modelos")],
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: true,
-    },
-  },
-});
+// const sequelize = new Sequelize(URL, {
+//   dialect: "postgres",
+//   models: [path.join(__dirname, "modelos")],
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: true,
+//     },
+//   },
+// });
 
-// Define la función main
-async function main() {
-  try {
-    await sequelize.authenticate();
-    console.log("Conexión con la base de datos establecida exitosamente.");
+// // Define la función main
+// async function main() {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Conexión con la base de datos establecida exitosamente.");
 
-    // Sincroniza los modelos con la base de datos
-    await sequelize.sync();
-    console.log("Modelos sincronizados con la base de datos.");
+//     // Sincroniza los modelos con la base de datos
+//     await sequelize.sync();
+//     console.log("Modelos sincronizados con la base de datos.");
 
-    // Accede a los modelos después de que la conexión se haya establecido
-    const modelos = sequelize.models;
-    Object.keys(modelos).forEach(async (modelName) => {
-      const instances = await modelos[modelName].findAll();
-      console.log(`Modelo: ${modelName}, Instancias: ${instances.length}`);
-    });
-  } catch (error) {
-    console.error("No se pudo conectar con la base de datos:", error);
-  }
-}
+//     // Accede a los modelos después de que la conexión se haya establecido
+//     const modelos = sequelize.models;
+//     Object.keys(modelos).forEach(async (modelName) => {
+//       const instances = await modelos[modelName].findAll();
+//       console.log(`Modelo: ${modelName}, Instancias: ${instances.length}`);
+//     });
+//   } catch (error) {
+//     console.error("No se pudo conectar con la base de datos:", error);
+//   }
+// }
 
-// Exporta tanto la instancia de Sequelize como la función main
-export { sequelize, main };
+// // Exporta tanto la instancia de Sequelize como la función main
+// export { sequelize, main };
 
 
 
