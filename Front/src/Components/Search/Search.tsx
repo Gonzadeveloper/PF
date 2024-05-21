5e018a58530da27abeab4299267248e6890de263
alvarorojas1101 <alvarorojas1101@gmail.com>
2024-05-21 16:22:53 -0300

    Actualizacion

--- Front/src/Components/Search/Search.tsx
+++ Front/src/Components/Search/Search.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import Card from '../Card/Card';
-import { RootState } from '../../Redux/index';
-
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Card from "../Card/card";
+import { RootState } from "../../Redux/index";
 
 function Search() {
   const products = useSelector((state: RootState) => state.products.products);
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 12; 
+  const itemsPerPage = 12;
 
   // Calcula los productos que se mostrarán en la página actual
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -53,19 +52,29 @@ function Search() {
           </div>
           <nav aria-label="Page navigation example">
             <ul className="pagination">
-              <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+              <li
+                className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
                 <button className="page-link" onClick={handlePrevious}>
                   &laquo;
                 </button>
               </li>
               {Array.from({ length: totalPages }, (_, index) => (
-                <li key={index + 1} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
-                  <button className="page-link" onClick={() => handleClick(index + 1)}>
+                <li
+                  key={index + 1}
+                  className={`page-item ${
+                    currentPage === index + 1 ? "active" : ""
+                  }`}>
+                  <button
+                    className="page-link"
+                    onClick={() => handleClick(index + 1)}>
                     {index + 1}
                   </button>
                 </li>
               ))}
-              <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+              <li
+                className={`page-item ${
+                  currentPage === totalPages ? "disabled" : ""
+                }`}>
                 <button className="page-link" onClick={handleNext}>
                   &raquo;
                 </button>
--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom' // routes
-import 'bootstrap/dist/css/bootstrap.min.css' // bootstrap 
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Provider } from "react-redux";
 import store from "./Redux/index.ts";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
-      <App />
-      </BrowserRouter>
-    </React.StrictMode>,
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+          audience: audience,
+        }}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Auth0Provider>
+    </React.StrictMode>
   </Provider>
-)
+);
