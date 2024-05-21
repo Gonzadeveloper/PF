98441e9a429a96d9d14fab48cfedd9f788b074e4
Gonzalo Cayssials <95725500+Gonzadeveloper@users.noreply.github.com>
2024-05-21 15:36:02 -0300

    Update index.ts

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -12,7 +12,7 @@ app.use(cors({
   }));
 
   app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'https://pf-vrse-m7h47rkpe-gonzadevelopers-projects.vercel.app'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
