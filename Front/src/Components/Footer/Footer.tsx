e0246b3bccf28341de7c0f48c434a0b3b97349e0
MatiasPeusco <matupeusco@gmail.com>
2024-05-22 16:07:46 -0300

    Footer Fixeado

--- Front/src/Components/Footer/Footer.tsx
+++ Front/src/Components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="bg-dark text-white py-3" style={{ backgroundColor: '#212529' }}>
+        <footer className="bg-dark text-white py-3 fixed-bottom">
             <div className="container-fluid">
                 <div className="row align-items-center justify-content-between">
                     <div className="col-md-6">
@@ -9,22 +9,22 @@ const Footer = () => {
                     <div className="col-md-6 d-flex justify-content-end">
                         <ul className="list-inline mb-0 d-flex align-items-center">
                             <li className="list-inline-item me-2">
-                                <a className="text-body-secondary btn btn-transparent" href="https://github.com/Gonzadeveloper/PF/tree/desarrollo" target="_blank">
+                                <a className="text-body-secondary btn btn-transparent" href="https://github.com/Gonzadeveloper/PF/tree/desarrollo" target="_blank" rel="noopener noreferrer">
                                     <img src="https://static-00.iconduck.com/assets.00/social-github-icon-256x250-yv67pnv6.png" alt="Github" style={{ width: '40px' }} />
                                 </a>
                             </li>
                             <li className="list-inline-item me-2">
-                                <a className="text-body-secondary btn btn-transparent" href="https://es.react.dev/" target="_blank">
+                                <a className="text-body-secondary btn btn-transparent" href="https://es.react.dev/" target="_blank" rel="noopener noreferrer">
                                     <img src="https://img.icons8.com/?size=80&id=wPohyHO_qO1a&format=png" alt="React" style={{ width: '40px' }} />
                                 </a>
                             </li>
                             <li className="list-inline-item me-2">
-                                <a className="text-body-secondary btn btn-transparent" href="https://redux.js.org/" target="_blank">
+                                <a className="text-body-secondary btn btn-transparent" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
                                     <img src="https://cdn.worldvectorlogo.com/logos/redux.svg" alt="Redux" style={{ width: '40px' }} />
                                 </a>
                             </li>
                             <li className="list-inline-item">
-                                <a className="text-body-secondary btn btn-transparent" href="https://www.typescriptlang.org/" target="_blank">
+                                <a className="text-body-secondary btn btn-transparent" href="https://www.typescriptlang.org/" target="_blank" rel="noopener noreferrer">
                                     <img src="https://www.svgrepo.com/show/374144/typescript.svg" alt="TypeScript" style={{ width: '40px' }} />
                                 </a>
                             </li>
