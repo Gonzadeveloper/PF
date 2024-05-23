7987b71b4f8d79bd23ffbd8727c2d8e9d00e5c1b
CEQuinterogarcia <kamterogarcia11@gmail.com>
2024-05-23 13:44:25 -0500

    eliminar back

--- Back/src/routes/getProductById.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getProductById }  from '../controllers/getProductById';
-
-const router = express.Router();
-
-// Ruta para buscar productos por nombre
-router.get('/products/:id', getProductById);
-
-export default router;
