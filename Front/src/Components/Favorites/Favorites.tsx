252ea34c14bf5dfe6adbbff336e94b757811df73
MatiasPeusco <matupeusco@gmail.com>
2024-05-24 18:14:28 -0300

    Agregue funcionalidad de Favoritos

--- Front/src/Components/Favorites/Favorites.tsx
+++ Front/src/Components/Favorites/Favorites.tsx
@@ -1,9 +1,13 @@
-function  Favorites () {
+import React from 'react';
+import FavoritesList from '../../Components/FavoritesList/FavoritesList'; // Importamos el componente FavoritesList
+
+const Favorites: React.FC = () => {
     return (
         <div className="container">
-            <h1>FAvoritos</h1>
-        </div>   
+            <h1>Mis Favoritos</h1>
+            <FavoritesList /> {/* Renderizamos el componente FavoritesList */}
+        </div>
     );
-}
+};
 
 export default Favorites;
