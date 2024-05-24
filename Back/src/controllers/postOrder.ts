25e0b71d205b3713c6b9b963b089ab42eeff7f8f
MGoretta <maugoretta@gmail.com>
2024-05-24 11:32:57 -0300

    rutas y CRUD de Order

---
+++ Back/src/controllers/postOrder.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { Order } from '../models/Order';
+
+const postOrder = async (req: Request, res: Response): Promise<void> => {
+  const orderData = req.body;
+
+  try {
+    // Validar los datos de entrada
+    if (!orderData.userId || !orderData.orderDate || !orderData.status) {
+      res.status(400).json({ message: 'Todos los campos son obligatorios' });
+      return;
+    }
+
+    // Crear una nueva orden
+    const newOrder = await Order.create(orderData as any);
+
+    res.status(201).json(newOrder);
+  } catch (error) {
+    console.error('Error creating order:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export { postOrder };
--- Back/src/routes/index.ts
+++ Back/src/routes/index.ts
@@ -2,6 +2,7 @@ import express from "express"
 import routesProduct  from './routesProduct';
 import routesUser  from './routesUser';
 import routesCategory  from './routesCategory';
+import routesOrder from './routesOrder'
 
 const router = express.Router()
 
@@ -9,6 +10,7 @@ const router = express.Router()
 router.use('/product', routesProduct)
 router.use('/user', routesUser)
 router.use('/category', routesCategory)
+router.use('/order', routesOrder)
 
 
 export default router;
