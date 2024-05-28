d428838296104c1f9ff2ac13cfbdbddc0229cbbe
CEQuinterogarcia <kamterogarcia11@gmail.com>
2024-05-27 19:08:27 -0500

    elimi

--- Back/build/Auth/config/routeAuth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const authController = require('./verificacionToken');
-const verificacionToken_1 = require("./verificacionToken");
-const router = express.Router();
-router.post('/', verificacionToken_1.checkJwt, authController.authenticateUser);
-module.exports = router;
