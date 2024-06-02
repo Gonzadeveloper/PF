ac37e50521bd882d7604d110895e98f8c5fb764d
alvarorojas1101 <alvarorojas1101@gmail.com>
2024-06-02 17:23:07 -0300

    home

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -5,22 +5,21 @@ import { useEffect } from "react";
 import Favorites from "./Components/Favorites/Favorites";
 import MyShopping from "./Components/MyShopping/My shopping";
 import Notifications from "./Components/Notifications/Notifications";
-import { Cart } from "./Components/ShoppingCart/ShoppingCart"
+import { Cart } from "./Components/ShoppingCart/ShoppingCart";
 import Home from "./Components/Home/Home";
 import Search from "./Components/Search/Search";
 import Help from "./Components/Help/Help";
 import ProductDetail from "./Components/Detail/Detail";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import Registrar from "./Components/Registrar/Registrar";
-import NewProduct from './Components/NewProduct/NewProduct'
+import NewProduct from "./Components/NewProduct/NewProduct";
 import { getAllProds } from "./Redux/Actions/productActions";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getAllProds())
+    dispatch(getAllProds());
   }, [dispatch]);
 
   return (
@@ -37,8 +36,7 @@ function App() {
           <Route path="/Help" element={<Help />} />
           <Route path="/Search" element={<Search />} />
           <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/registrar" element={<Registrar />} />
-          <Route path='/newproduct' element={<NewProduct/>}/>
+          <Route path="/newproduct" element={<NewProduct />} />
         </Routes>
       </div>
       <Footer />
