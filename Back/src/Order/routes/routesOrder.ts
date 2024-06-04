15515e6645b6dec28ea5833bc30803839ac1304a
MGoretta <maugoretta@gmail.com>
2024-06-04 00:20:45 -0300

    getOrderByUser incluida en routesOrder en Order, y tamb reviewController para filtro por usuario y por product

--- Back/src/Order/routes/routesOrder.ts
+++ Back/src/Order/routes/routesOrder.ts
@@ -3,13 +3,14 @@ import { deleteOrder } from "../controllers/deleteOrder";
 import { postOrder } from "../controllers/postOrder";
 import { getAllOrder } from "../controllers/getAllOrder";
 import { putOrder } from "../controllers/putOrder";
-
+import { getOrderByUser } from '../controllers/getOrderByUser';
 
 const router = express.Router()
 
 
 router.post ('/', postOrder);
 router.get ('/', getAllOrder);
+router.get('/user/:idUser', getOrderByUser);
 router.delete('/:id', deleteOrder);
 router.put ('/:id', putOrder);
 
