f35db0e5cbfb87f60a50da94f0c11126f69149c1
CEQuinterogarcia <kamterogarcia11@gmail.com>
2024-06-04 09:49:35 -0500

    review

--- Back/src/Review/routes/routesReview.ts
+++ Back/src/Review/routes/routesReview.ts
@@ -2,18 +2,20 @@ import express from "express"
 // import { deleteReview } from "../controllers/deleteReview";
 import { postReview } from "../controllers/postReview";
 import { deleteReview } from "../controllers/deleteReview";
- import { getAllReview } from "../controllers/getAllReview";
- import { getProductReviewAverage, getUserReviewAverage } from '../controllers/reviewController'
+import { getAllReview } from "../controllers/getAllReview";
+import { getProductReviewAverage, getUserReviewAverage } from '../controllers/reviewController'
+import { getReviewByIdProduct } from "../controllers/getReviewByIdProduct";
 // import { putOReview } from "../controllers/putReview";
 
 
 const router = express.Router()
 
 
- router.post ('/', postReview);
- router.get ('/', getAllReview);
- router.delete('/:id', deleteReview);
- router.get('/product/:productId/average-rating', getProductReviewAverage);
+router.post('/', postReview);
+router.get('/user/:id', getReviewByIdProduct);
+router.get('/', getAllReview);
+router.delete('/:id', deleteReview);
+router.get('/product/:productId/average-rating', getProductReviewAverage);
 router.get('/user/:userId/average-rating', getUserReviewAverage);
 // router.put ('/:id', putOReview);
 
